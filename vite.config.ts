323f61d828e35e587c007f1114a78221aaf3c01a
yejiawen <2674199819@qq.com>
2026-03-24 10:33:41 +0800

    chore: 更新环境配置和构建设置
    - 将API基础URL从IP地址更改为域名
    - 添加新的忽略目录到.gitignore
    - 更新vite配置以优化构建输出

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,28 @@ export default defineConfig({
         changeOrigin: true
       }
     }
+  },
+  build: {
+    // 生成静态资源的目录名
+    assetsDir: 'assets',
+    // 设置最终包的输出目录
+    outDir: 'yjwblogs.top_8002',
+    // 是否生成 source map 文件
+    sourcemap: false,
+    // 分割代码
+    chunkSizeWarningLimit: 2000, // 分割代码块的大小限制（单位kb）
+    // 压缩配置
+    terserOptions: {
+      compress: {
+        drop_console: true, // 删除console
+        drop_debugger: true // 删除debugger
+      }
+    },
+    // 启用/禁用 CSS 代码拆分
+    cssCodeSplit: true,
+    // 构建后是否清除输出目录
+    emptyOutDir: true,
+    // 构建时是否重写 import 的路径（例如，将相对路径转为绝对路径）
+    reportCompressedSize: true,
   }
 })
